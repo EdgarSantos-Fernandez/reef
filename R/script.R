c1f515a31abcdbf8b5d5be2becc5295886b8e096
ed_sans <edgar.santosfdez@gmail.com>
2021-04-12 16:18:46 +1000

    Add files via upload

---
+++ R/script.R
@@ -0,0 +1,12 @@
+#' @title reef dataset
+#' @description This data set contains manual classification data from underwater images. 
+#' @format A data frame.
+#' @source
+#' @return A data frame
+#' @importFrom 
+#' @export
+#' @author Edgar Santos-Fernandez
+#' @examples
+#' data('reef')
+#' summary(reef)
+
