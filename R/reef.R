b642037b9769676dea4a53d89e3b61ef94b42e89
EdgarSantos-Fernandez <edgar.santosfdez@gmail.com>
2021-04-12 17:53:55 +1000

    Initial commit

---
+++ R/reef.R
@@ -0,0 +1,18 @@
+#' reef data set
+#'
+#' Data associated with the article: Complex image classification via crowdsourcing for conservation: a viable solution?
+#'
+#' @docType data
+#'
+#' @usage data(reef)
+#'
+#' @format A dataframe
+#'
+#' @keywords datasets
+#'
+#'
+#' @source \href{https://github.com/EdgarSantos-Fernandez/reef}
+#'
+#' @examples
+#' data('reef')
+#' summary(reef)
